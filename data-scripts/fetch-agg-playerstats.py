dcbf1d13902e7a41c9e8268d5903f6aac8ca9a32
Evan Feng <evanfeng55@gmail.com>
2016-05-30 18:25:46 -0400

    aggregate player stats

--- data-scripts/fetch-agg-playerstats.py
+++ data-scripts/fetch-agg-playerstats.py
@@ -1,12 +1,19 @@
 # Creates a JSON file containing full player stats for a given season
 
 # USAGE: python fetch-agg-playerstats [season] [season-type] [ids (optional)]
-# EXAMPLE: python fetch-agg-playerstats.py 2015-16 'Regular Season' '203092,203112'
+
+# EXAMPLE: (list of players)
+# python fetch-agg-playerstats.py 2015-16 'Regular Season' '203092,203112'
+
+# EXAMPLE: (all players)
+# python fetch-agg-playerstats.py 2015-16 'Regular Season' 
+
 
 import requests
 import csv
 import sys
 import json
+import time
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -22,7 +29,7 @@ else:
 	# print(request_url + "Season=" + season);
 
 
-	if sys.argv[3] is None:
+	if len(sys.argv) == 3:
 		# get me all active players for the specified season
 		url_allPlayers = (request_url + "Season=" + season) 		
 		response = requests.get(url_allPlayers, headers=headers)
@@ -44,43 +51,42 @@ else:
 	output['data'] = []
 
  
- 	j = 0
 	for i in ids:
 
-		if(j < 5):
+		# construct playerprofile request url
 
-			# construct playerprofile request url
+		player_request_url = 'http://stats.nba.com/stats/playerprofile?'
+		season = sys.argv[1]
+		season_type = sys.argv[2]
+		league_id = "00"
+		graph_start = sys.argv[1]
+		graph_end = sys.argv[1]
+		graph_stat = "PTS"
 
-			player_request_url = 'http://stats.nba.com/stats/playerprofile?'
-			season = sys.argv[1]
-			season_type = sys.argv[2]
-			league_id = "00"
-			graph_start = sys.argv[1]
-			graph_end = sys.argv[1]
-			graph_stat = "PTS"
+		url_player_profile = (player_request_url + 'PlayerID='+str(i) + "&SeasonType="+season_type + '&Season='+season + '&LeagueID='+league_id + '&GraphStartSeason='+graph_start + '&GraphEndSeason='+graph_end + '&GraphStat='+graph_stat)
+		print('fetching... ' + url_player_profile + '\n');
 
-			url_player_profile = (player_request_url + 'PlayerID='+str(i) + "&SeasonType="+season_type + '&Season='+season + '&LeagueID='+league_id + '&GraphStartSeason='+graph_start + '&GraphEndSeason='+graph_end + '&GraphStat='+graph_stat)
-			print('fetching... ' + url_player_profile + '\n');
+		# make request and get season averages from response 
 
-			# make request and get season averages from response 
+		player_response = requests.get(url_player_profile, headers=headers)
+		player_response.raise_for_status()
 
-			player_response = requests.get(url_player_profile, headers=headers)
-			player_response.raise_for_status()
+		profile_stats = player_response.json()['resultSets'][0]['rowSet']
 
-			profile_stats = player_response.json()['resultSets'][0]['rowSet']
+		player_obj = {}
+		player_obj['player_id'] = i
+		player_obj['player_name'] = profile_stats[0][1]
+		player_obj['stats'] = profile_stats[0]
 
-			player_obj = {}
-			player_obj['player_id'] = i
-			player_obj['player_name'] = profile_stats[0][1]
-			player_obj['stats'] = profile_stats[0]
+		output['data'].append(player_obj)
 
-			output['data'].append(player_obj)
+		# wait for 5 seconds before fetching the next player
+		time.sleep(5)
+		print('waiting for 5 seconds... ')
 
 
-		j+=1
 
 	# output json object to file 
-
 	with open('../data-local/agg-playerstats/agg-playerstats_' + season + '.json', 'w') as outfile:
 		json.dump(output, outfile)
 
