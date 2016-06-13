20c84e19132c85d89cc0cfe3e858e4c351807116
Evan Feng <evanfeng55@gmail.com>
2016-06-13 00:16:05 -0400

    resize

--- src/charts/three-and-d.js
+++ src/charts/three-and-d.js
@@ -8,7 +8,8 @@ import chartUtil from '../util/chart-util';
 var ThreeAndD = React.createClass({
 
   getInitialState: function (){
-    return { season_select: "2015-16", team_select: "All Teams", position_select: "SG/SF", team_obj: {}, data_obj: {}};
+    return { season_select: "2015-16", team_select: "All Teams", position_select: "SG/SF", min3pm_select: 0.0,
+              team_obj: {}, data_obj: {}};
   },
 
   componentDidMount: function(){
@@ -31,18 +32,28 @@ var ThreeAndD = React.createClass({
   },
 
   componentWillUpdate: function(nextProps, nextState){
-    this.drawChart(nextState.data_obj.data, nextState.team_select, nextState.position_select);
+    this.drawChart(nextState.data_obj.data, nextState.team_select, nextState.position_select, nextState.min3pm_select);
   },
 
 
-  drawChart: function(data, teamselect, positionselect){
+  drawChart: function(unfiltered_data, teamselect, positionselect, min3pmselect){
     d3.select("svg").remove();
 
-    console.log("drawChart()");
-    console.log(data);
+    //filter data
+    var data = [];
+    unfiltered_data.forEach(function(datum){
+      // filter by team and position
+      if( (datum.team == teamselect || teamselect == "All Teams") && 
+          (chartUtil.containsPosition(datum.position, positionselect) || positionselect == "All Positions") &&
+          (datum.stats[10] >= min3pmselect) ){
+
+        data.push(datum)
+      
+      }
+    }.bind(this))
 
     // chart dimensions
-    var p = 50, w = 960, h = 450;    
+    var p = 40, w = 930, h = 420;    
 
     // add canvas element
     var svg = d3.select("#chart1-3andd").append("svg")
@@ -80,7 +91,7 @@ var ThreeAndD = React.createClass({
     console.log("MAX DRating: " + d3.max(data, xVal))
 
     var xScale = d3.scale.linear().domain([d3.min(data, xVal), d3.max(data, xVal)]).range([0, w]);
-    var yScale = d3.scale.linear().domain([d3.min(data, yVal), d3.max(data, yVal)]).range([h, 0]);
+    var yScale = d3.scale.linear().domain([Math.max(d3.min(data, yVal)-0.02, 0), Math.min(d3.max(data, yVal)+0.02, 1.0)]).range([h, 0]);
     var circleRadius = function(mpg){
       return mpg/30 * 7 + 1; 
     }
@@ -119,35 +130,29 @@ var ThreeAndD = React.createClass({
     // add dots for all elements in the dataset 
     data.forEach(function(datum){
 
-      // filter by team and position
-      if( (datum.team == teamselect || teamselect == "All Teams") && 
-          (chartUtil.containsPosition(datum.position, positionselect) || positionselect == "All Positions") ){
+      svg.append("circle")
+        .attr("class", "dot")
+        .attr("r", circleRadius(datum.stats[6]))
+        .attr("cx", xScale(xVal(datum)))
+        .attr("cy", yScale(yVal(datum)))
+        .style("fill", chartUtil.getTeamColors(datum.team).primary)
+        .style("stroke", chartUtil.getTeamColors(datum.team).secondary)
 
-        svg.append("circle")
-          .attr("class", "dot")
-          .attr("r", circleRadius(datum.stats[6]))
-          .attr("cx", xScale(xVal(datum)))
-          .attr("cy", yScale(yVal(datum)))
-          .style("fill", chartUtil.getTeamColors(datum.team).primary)
-          .style("stroke", chartUtil.getTeamColors(datum.team).secondary)
-
-          .style("opacity", .7)
-          .on("mouseover", function(){
-              tooltip.transition()
-                .duration(100)
-                .style("opacity", 1);
-              tooltip.html(chartUtil.getTooltipHTML(datum))
-                .style("left", (d3.event.pageX - 230) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-          })
-          .on("mouseout", function(){
+        .style("opacity", .7)
+        .on("mouseover", function(){
             tooltip.transition()
-            .duration(100)
-            .style("opacity", 0);
-          }.bind(this));
+              .duration(100)
+              .style("opacity", 1);
+            tooltip.html(chartUtil.getTooltipHTML(datum))
+              .style("left", (d3.event.pageX - 230) + "px")
+              .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(){
+          tooltip.transition()
+          .duration(100)
+          .style("opacity", 0);
+        }.bind(this));
 
-      }
-      
     }.bind(this));
 
   }, 
@@ -157,6 +162,10 @@ var ThreeAndD = React.createClass({
   },
   onSelectPosition: function(event){
     this.setState({position_select: event.target.value});
+  },
+  onSelectMin3PM: function(event){
+    var min3pm = event.target.value.split('>=')[1].split(' ')[1];
+    this.setState({min3pm_select: parseFloat(min3pm) });
   },
 
   render: function() {
@@ -195,6 +204,16 @@ var ThreeAndD = React.createClass({
             <option>SF</option>
             <option>PF</option>
             <option>C</option>
+          </select>
+
+          <select defaultValue="> 0.0 3P Made" onChange={this.onSelectMin3PM}>
+            <option>>= 0.0 3PM/Game</option>
+            <option>>= 0.5 3PM/Game</option>
+            <option>>= 1.0 3PM/Game</option>
+            <option>>= 1.5 3PM/Game</option>
+            <option>>= 2.0 3PM/Game</option>
+            <option>>= 2.5 3PM/Game</option>
+            <option>>= 3.0 3PM/Game</option>
           </select>
 
 
